2b489465cfcda9a87790d72db984df3658146ac7
Sienna <remicknsisi@gmail.com>
2022-08-23 11:17:17 -0400

    practice setting up click alert to Contact Me button

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Contact Me!";
+h2.textContent = "this is added by JS";
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
